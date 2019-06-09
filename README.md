d0a2bf216bf173b4f85c044825e8a2da97e318c6
Mat Werber <werberm@amazon.com>
2019-06-08 23:04:48 -0400

    update

--- README.md
+++ README.md
@@ -1,67 +1,72 @@
+# **Note: this is a work in process, not yet**
+
+Some components are not yet functional and/or in early draft stage. 
+
 # aws-alexa-smart-home-demo
 
-**Forgive the mess, this is a work in process, far from complete. Not yet ready to deploy!**
+Many of the existing Alexa Smart Home skill examples focus only on the Alexa skill's Lambda function and typically give hard-coded examples of typical JSON requests and responses. 
 
-**THIS README, THE COMMENTS/DOCS/ETC ARE NOT COMPLETE / CORRECT (YET)!**
+This project aims to quickly give you a more realistic, end-to-end smart home skill example with the following core components: 
 
-The current Alexa skill documentation provides solid Alexa API documentation and
-example Lambda functions with hard-coded JSON input events and responses, but 
-they do not provide a full, 'end-to-end' dynamic project. 
+1. Real (developer user) sign-up via an Alexa-enabled smart speaker (e.g. an [Echo Dot](https://www.amazon.com/All-new-Echo-Dot-3rd-Gen/dp/B0792KTHKJ)), [Alexa web app](https://alexa.amazon.com), Alexa mobile app ( [Android](https://play.google.com/store/apps/details?id=com.amazon.dee.app&hl=en_US), [iOS](https://itunes.apple.com/us/app/amazon-alexa/id944011620?mt=8)), or the test tool from the [Alexa Developer Console](https://developer.amazon.com/alexa/console/ask/). 
 
-The goal of this project is to provide a fast, easy way to deploy an end-to-end
-solution that includes these key features: 
+2. [An AWS IoT Core "Thing"](https://docs.aws.amazon.com/iot/latest/developerguide/iot-thing-management.html) that is a logical cloud representation of a physical smart home device. Your Alexa skill will send commands to or read device state from the IoT Thing. Your physical device will receive commands from or send device state updates to the IoT Thing. 
 
-1. Alexa Skill created in the Alexa Developer Console [developer.amazon.com/alexa/console/ask](developer.amazon.com/alexa/console/ask)
-2. Cognito User Pool configured as the identity provider (IdP) for the Alexa skill
-3. An AWS IoT Core "Thing" created in the IoT Registry
-4. DynamoDB table to store a mapping of IoT Things associated to our Cognito users
-5. Lambda linked to our Alexa skill that handles all core Alexa logic, such as: 
-    * Querying DynamoDB to discover devices available to our user
-    * Taking user input from Alexa and updating the IoT Thing's device shadow
-    * Querying device state from IoT thing's device shadow and returning to Alexa
-6. OPTIONAL - Guide to link AWS IoT Thing's device shadow to LEDs on an ESP32 board
+3. A [Cognito user pool](https://docs.aws.amazon.com/cognito/latest/developerguide/cognito-user-identity-pools.html) that acts as the identity provider (IdP) for your skill and stores the credentials and attributes of your signed-up users. 
 
-This project assumes that you manually associate an IoT Thing to a skill user
-by adding a record to the application's DynamoDB table; a mockup of the user-device
-registration process is not included. 
+4. An [Amazon DynamoDB](https://aws.amazon.com/dynamodb/) table that contains a mapping between IoT Things and their Cognito user pool ID. 
+
+5. A main [AWS Lambda](https://aws.amazon.com/lambda/) function that handles interaction between the AWS-managed Alexa service and your customer-managed AWS cloud backend resources listed above. 
+
+6. OPTIONAL - An [ESP32](https://www.amazon.com/HiLetgo-ESP-WROOM-32-Development-Microcontroller-Integrated/dp/B0718T232Z) with some LEDs and temp/humidity sensor that is linked to your AWS IoT Thing. The LEDs react to commands you give to Alexa and the device sends temp & humidity readings back to the IoT Thing which Alexa can provide to a user upon request. 
 
 ## Prerequisites
 
-1. AWS Account
-2. IAM user access with admin privileges
-3. S3 Bucket to store project files
-
-
-### OPTIONAL - Hardware
-
-This project includes an optional hardware setup that includes an ESP32 chip with LEDs and a temp/humidity sensor. Though not required, you may link this device to your AWS IoT and Alexa backend for a more realistic Smart Home experience. 
-
-1. Read/Write access to an existing S3 bucket, to store CloudFormation templates.
-2. ESP32 
-3. breadboard and jumper wires
-4. Three LEDs of different color (preferably red, white, blue)
-3. One DSP11 temperature/humidity sensor
-4. Resistors: three of X ohms, one of Y ohms
-5. Mobile phone with Amazon Alexa app installed
-6. OPTIONAL - Alexa-enabled smart speaker (e.g. Echo, Echo Dot, Echo Show, etc.)
+1. AWS Account & admin user or role access
+2. S3 Bucket (to store CloudFormation template)
+3. [Node10.x](https://nodejs.org/en/download/) and [npm](https://www.npmjs.com/get-npm) (to install Lambda dependencies before uploading to AWS)
 
 ## Getting Started
 
-1. Register a developer account with the [Alexa Developer Console](https://developer.amazon.com/).
-
-2. Install and initialize the Alexa CLI, following **steps 1 to 3** in the [Alexa CLI quick start documentation](https://developer.amazon.com/docs/smapi/quick-start-alexa-skills-kit-command-line-interface.html).
-
-    * **Note:** When you run `ask init` on step 3, choose **No** when asked **"do you want to host your skill's backend code in AWS Lambda?"**. Choosing 'yes' is needed if you want the Alexa service to host your Lambda functions... however, this special hosting option is not available for the Smart Home skills kit. We will instead be hosting Lambda functions (and other AWS resources) in *your* AWS account, which is why we can say "no" to the ASK CLI.
-
-3. (Install the SAM CLI)[https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html]
+1. Register a developer account with the [Alexa Developer Console](https://developer.amazon.com/). Note that the **email address** you use should match the email address you later plan to test your skill with. You could optionally complete additional steps to open up testing to others, but this demo does not cover that and focuses on testing by one user (you). 
 
 2. Navigate to the [Alexa Skills Kit (ASK) Dashboard](https://developer.amazon.com/alexa/console/ask) and click **Create Skill**.
 
-3. Give your skill a name, such as **smart-home-demo** and select **Smart Home** as the skill model: 
+3. Give your skill a name, such as **alexa-smart-home-demo** and select **Smart Home** as the skill model. 
 
     ![name_alexa_skill]
 
-[name_alexa_skill]: ./images/name_alexa_skill.png
+    [name_alexa_skill]: ./images/name_alexa_skill.png
+
+4. Click **Create Skill**. You will be taken to a configuration page. We need to first create additional resources before we use their values to complete this page. For now, copy your **Skill ID** (e.g. amzn1.ask.skill.xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx) into a text editor. You will need this value later.
+
+5. Navigate to [https://developer.amazon.com/settings/console/mycid](https://developer.amazon.com/settings/console/mycid) and copy your **Alexa Vendor ID** and **Customer ID** in a text editor along with your Skill ID. 
+
+6. Your Alexa skill ID, vendor ID, and customer ID are needed to configure your skill's backend resources. As these are values are considered secrets and should not be shared, we will store them in [AWS Secrets Manager](https://aws.amazon.com/secrets-manager/) and dynamically retrieve them when needed in order to keep them safe:
+
+    1. Log in to your AWS account and navigate to the [AWS Secrets Manager Console](https://console.aws.amazon.com/secretsmanager/).
+    2. Click **Store a new secret**.
+    3. Under **Select secret type**, select **Other type of secret**.
+    4. Specify three key value pairs with the key names of **SkillId**, **VendorId**, and **CustomerId** and with the values you copied earlier from the Alexa Developer Console. **Note:** key names are case sensitive and must match those shown below in order to work with the CloudFormation template we will launch later:
+
+        ![create_secret_1]
+
+        [create_secret_1]: ./images/create_secret_1.png
+
+    5. Accept the default encryption key and click **Next**.
+
+    6. For **Secret name**, use the value **AlexaSmartHomeSkillCredentials**. Use this exact value to avoid the need to change the CloudFormation template we will launch later. Optionally enter a brief **Description** and click **Next**:
+
+        ![create_secret_2]
+
+        [create_secret_2]: ./images/create_secret_2.png
+
+    7. Accept the default value **Disable automatic rotation** and click **Next**.
+    8. On the final review page, click **Store** to save your secret. 
+
+2. 
+
+3. (Install the SAM CLI)[https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html]. We use this CLI in our deployment script to create and launch a CloudFormation script containing the bulk of your needed resources. 
 
 
 4. Edit **deploy.sh** and set the BUCKET variable to the name of an S3 bucket to use for storing later CloudFormation templates. 
--- lambda/functions/alexa-skill/index.js
+++ lambda/functions/alexa-skill/index.js
@@ -9,169 +9,105 @@ const discoveryConfig = require ("./discoveryConfig");
 const iotdata = new AWS.IotData({ endpoint: IOT_ENDPOINT });
 const iot = new AWS.Iot();
 const lambda = new AWS.Lambda();
-
-// Set to true when debugging with a hard-coded user ID / token for testing.
-const debugMode = false;
-
+const REQUIRED_PAYLOAD_VERSION = "3";
 
 exports.handler = async function (request, context) {
 
     try {
-        log("DEBUG: ", "Alexa request:\n", JSON.stringify(request, null, 2));
-        log("DEBUG: ", "Alexa context:\n", JSON.stringify(context,null, 2));
-        
-        if (!('directive' in request)) {
+        log("Alexa request:\n", request);
+        log("Alexa context:\n", context);
 
-            log("ERROR: ", `Request is missing 'directive' key.`);
-    
-            let alexaErrorResponse = new AlexaResponse(
-                {
-                    "name": "ErrorResponse",
-                    "payload": {
-                        "type": "INVALID_DIRECTIVE",
-                        "message": "Missing key: directive, Is request a valid Alexa directive?"
-                    }
-                }
+        if (!('directive' in request)) {
+            return sendErrorResponse(
+                'INVALID_DIRECTIVE',
+                'directive is missing from request'
             );
-            
-            return sendResponse(alexaErrorResponse.get());
         }
-    
-        if (request.directive.header.payloadVersion !== "3") {
         
-            log("ERROR: ", `Request's payload version is `
-                + `${request.directive.header.payloadVersion} but this function `
-                + `requires version 3.`
+        let header = request.directive.header;
+        let namespace = header.namespace; 
+        // This was in an example but its an odd statement 
+        //let namespace = ((request.directive || {}).header || {}).namespace;
+
+        if (header.payloadVersion !== REQUIRED_PAYLOAD_VERSION) {
+            return sendErrorResponse(
+                'INVALID_DIRECTIVE', 
+                `Payload v${header.payloadVersion} unsupported.`
+                + `Expected v${REQUIRED_PAYLOAD_VERSION}`
             );
-        
-            let alexaErrorResponse = new AlexaResponse(
-                {
-                    "name": "ErrorResponse",
-                    "payload": {
-                        "type": "INTERNAL_ERROR",
-                        "message": "This skill only supports Smart Home API version 3"
-                    }
-                }
-            );
-            
-            return sendResponse(alexaErrorResponse.get())
         }
-    
-        let namespace = ((request.directive || {}).header || {}).namespace;
-    
+        
+        var authResponse = await getAuthTokenValidationResponse(
+            request.directive.payload.scope.token
+        );
+        if (authResponse.hasOwnProperty('FunctionError')) {
+            let authError = JSON.parse(authResponse.Payload);
+            if (authError.errorMessage === 'Token is expired') {
+                return sendErrorResponse(
+                    'EXPIRED_AUTHORIZATION_CREDENTIAL', 
+                    `Auth token is expired`
+                );
+            }
+            else {
+                return sendErrorResponse(
+                    'INVALID_AUTHORIZATION_CREDENTIAL', 
+                    `Auth token invalid`
+                );
+            }
+        }
+
+        var authToken = JSON.parse(authResponse.Payload);
+
         if (namespace.toLowerCase() === 'alexa.discovery') {
-    
-            log("DEBUG: ", "Calling handleDiscovery()...");
-            let response = await handleDiscovery(request, context);
+            let response = await handleDiscovery(request, context, authToken);
             return sendResponse(response.get());
     
         }
         else if (namespace.toLowerCase() === 'alexa.thermostatcontroller') {
-    
-            log("DEBUG: ", "Calling handleThermostatControl()...");
             let response = await handleThermostatControl(request, context);
             return sendResponse(response.get());
-    
         }
-        else {
-    
-            log("ERROR: ", `${namespace} is an unsupported Alexa namespace.`);
-    
-            let alexaErrorResponse = new AlexaResponse(
-                {
-                    "name": "ErrorResponse",
-                    "payload": {
-                        "type": "INVALID_DIRECTIVE",
-                        "message": `Unsupported directive ${request.directive}.`
-                    }
-                }
+            
+        else {       
+            return sendErrorResponse(
+                'INVALID_DIRECTIVE', 
+                `Namespace ${namespace} is unsupported by this skill.`
             );
-
-            return sendResponse(alexaErrorResponse.get())
         }
     }
     catch (err) {
-        console.log('ERROR:\n' + err);
-        throw ({ Error: err });
+        log('ERROR:\n', err);
+        return sendErrorResponse(
+            'INTERNAL_ERROR', 
+            `Unhandled error: ${err}`
+        );
     }
 
 };
 
+async function handleDiscovery(request, context, authToken) {
 
-/*
-handleDiscovery is called by the Alexa service when the user asks Alexa (via
-voice or app) to discover devices and should return a list of devices associated
-to the user invoking the skill. Alexa passes an authentication token in the 
-request which we must validate against the identity provider (IdP) you have
-associated with the Smart Home skill in the Alexa Developer Console. If the 
-token is valid and has not expired, we can proceed by parsing the token to 
-obtain the user's ID and then making a call to our backend (e.g. AWS IoT Core, 
-and/or a database) to find the device(s) (aka "endpoints") associated to our
-user. We return the list of endpoints to Alexa. If an error occurs or no 
-endpoints exist, we must return an empty list. 
-*/
-async function handleDiscovery(request, context) {
-
-    let alexaResponse = new AlexaResponse({
-        "namespace": "Alexa.Discovery",
-        "name": "Discover.Response"
-    });
-
-    var authTokenValidationResponse = await getAuthTokenValidationResponse(
-        request.directive.payload.scope.token
-    );
-
-    if (authTokenValidationResponse.hasOwnProperty('FunctionError')) {
+    try {
+        log("Calling handleDiscovery()");
     
-        log("ERROR: ", "Authentication error:\n",
-            JSON.stringify(authTokenValidationResponse, null, 2)
-        );
-    
-        alexaResponse.setEndpointsToEmptyArrayDueToDiscoveryError();
-    
-    }
-    else {
-        log("DEBUG: ", "Auth token is valid.");
-        var authToken = JSON.parse(authTokenValidationResponse.Payload);
+        let alexaResponse = new AlexaResponse({
+            "namespace": "Alexa.Discovery",
+            "name": "Discover.Response"
+        });
+
         var userId = authToken.sub;
-
-        log("DEBUG: ", "Getting user endpoints...");
         var endpoints = await getUserEndpoints(userId);
-        log("DEBUG: ", "User endpoints:\n", JSON.stringify(endpoints));
-        
-        log("DEBUG: ", "Adding endpoints to payload response...");
+
         endpoints.forEach(endpoint => {
             alexaResponse.addPayloadEndpoint(endpoint);
         });
-    
+
+        return alexaResponse;
+    }
+    catch (err) {
+        throw ("handleDiscovery() failed: " + err);
     }
 
-    return alexaResponse;
-
-}
-
-/*
-This function receives a list of devices, each a JSON object, that are
-associated to our user. This function selects the subset of properties from that
-that list that are required by Alexa and pushes it into a new "endpoints" list
-that becomes part of our final handleDiscovery response. 
-*/
-// DEPRECATED
-function convertUserDevicesToEndpoints(userDevices) {
-    var endpoints = [];
-    for (var index in userDevices) {
-        var userDevice = userDevices[index];
-        var endpoint = {
-            endpointId: userDevice.thingName,
-            manufacturerName: userDevice.manufacturerName,
-            friendlyName: userDevice.friendlyName,
-            description: userDevice.description,
-            displayCategories: userDevice.displayCategories,
-            capabilities: userDevice.capabilities,
-        };
-        endpoints.push(endpoint);
-    }
-    return endpoints;
 }
 
 
@@ -181,30 +117,20 @@ provided by Alexa during invocation is valid and not expired.
 */
 async function getAuthTokenValidationResponse(token) {
 
-    var ignoreExpiredToken = false;
-
-    if (debugMode === true) {
-
-        log("DEBUG: ", "debugMode = true; expired auth tokens will be accepted"
-            + "so long as the rest of the token is ok."
-        );
-
-        ignoreExpiredToken = true;
+    try {
+        var params = {
+            FunctionName: VERIFY_COGNITO_TOKEN_FUNCTION, 
+            InvocationType: "RequestResponse", 
+            Payload: JSON.stringify({ token: token })
+        };
+    
+        let response = await lambda.invoke(params).promise();
+        return response;
+    }
+    catch (err) {
+        throw (`Error invoking ${VERIFY_COGNITO_TOKEN_FUNCTION}: ${err}`);
     }
 
-    var payload = JSON.stringify({
-        token: token,
-        ignoreExpiredToken: ignoreExpiredToken
-    });
-
-    var params = {
-        FunctionName: VERIFY_COGNITO_TOKEN_FUNCTION, 
-        InvocationType: "RequestResponse", 
-        Payload: payload
-    };
-
-    let response = await lambda.invoke(params).promise();
-    return response;
 
 }
 
@@ -214,8 +140,8 @@ token provided by Alexa in the function request and invokes another function
 that returns a list of all devices associated to that user. 
 */
 async function getUserEndpoints(userId) {
-//async function getUserDevices(userId) {
-    
+
+    log("Getting user endpoints");
     var payload = JSON.stringify({
         userId: userId
     });
@@ -226,12 +152,12 @@ async function getUserEndpoints(userId) {
         Payload: payload
     };
 
-    log("DEBUG: ", "Invoking Lambda: ", params);
+    log("Invoking Lambda: ", params);
     let getUserDevicesResponse = await lambda.invoke(params).promise();
 
-    log("DEBUG: ", "User Device Lambda response: ", getUserDevicesResponse);
+    log("User Device Lambda response: ", getUserDevicesResponse);
     var devices = (JSON.parse(getUserDevicesResponse.Payload)).deviceList;
-    log("DEBUG: ", "Devices: ", devices);
+    log("Devices: ", devices);
     /*
         response will contain: {
             thingName: "xxxx",
@@ -246,9 +172,9 @@ async function getUserEndpoints(userId) {
             thingName: device.thingName
         };
 
-        log("DEBUG: ", "Calling IoT.describeThing() with params: ", params);
+        log("Calling IoT.describeThing() with params: ", params);
         let iotDescription = await iot.describeThing(params).promise();
-        log("DEBUG: ", "IoT Description:\n", iotDescription);
+        log("IoT Description:\n", iotDescription);
         let thingConfig = discoveryConfig[iotDescription.attributes.modelNumber][iotDescription.attributes.firmwareVersion];
 
         let endpoint = {
@@ -262,18 +188,18 @@ async function getUserEndpoints(userId) {
 
         endpoints.push(endpoint);
     }
-
+    log("User endpoints:\n", endpoints);
     return endpoints;
 }
 
 /*
-Short-hand for logging messages...
+Short-hand for logging messages
 */
-function log(message, message1, message2) {
+function log(message1, message2) {
     if (message2 == null) {
-        console.log(message + message1);
+        console.log(message1);
     } else {
-        console.log(message + message1 + JSON.stringify(message2, null, 2));
+        console.log(message1 + JSON.stringify(message2, null, 2));
     }
 }
 
@@ -288,7 +214,7 @@ This function handles all requests that Alexa identifies as being a
 
 */
 async function handleThermostatControl(request, context) {
-
+    log("Calling handleThermostatControl()");
     let endpoint_id = request.directive.endpoint.endpointId;
     let token = request.directive.endpoint.scope.token;
     let correlationToken = request.directive.header.correlationToken;
@@ -307,7 +233,7 @@ async function handleThermostatControl(request, context) {
     );
 
     if (authTokenValidationResponse.hasOwnProperty('FunctionError')) {
-        log("ERROR: ", "Authentication error:\n", authTokenValidationResponse);
+        log("ERROR: Authentication error:\n", authTokenValidationResponse);
     } 
     else {
 
@@ -321,7 +247,7 @@ async function handleThermostatControl(request, context) {
             }
         );
         
-        log("DEBUG: ", `Request method is ${requestMethod}...`);
+        log(`Request method is ${requestMethod}`);
     
         if (requestMethod === 'SetTargetTemperature') {
             
@@ -354,9 +280,9 @@ async function handleThermostatControl(request, context) {
                 payload: JSON.stringify(shadowState) /* Strings will be Base-64 encoded on your behalf */, /* required */
                 thingName: endpointId /* required */
             };
-            log("DEBUG: ", `Updating shadow of ${endpointId}:\n`, shadowState);
+            log(`Updating shadow of ${endpointId}:\n`, shadowState);
             var updateShadowResponse = await iotdata.updateThingShadow(params).promise();
-            log("DEBUG: ", `Update shadow response:\n`, updateShadowResponse);
+            log(`Update shadow response:\n`, updateShadowResponse);
             
 
 
@@ -369,7 +295,7 @@ async function handleThermostatControl(request, context) {
             return alexaResponse.get();
         }
         else {
-            log("ERROR: ", `Unsupported request method ${requestMethod} for ThermostatController.`);
+            log(`ERROR: Unsupported request method ${requestMethod} for ThermostatController.`);
             return new AlexaResponse(
                 {
                     "name": "ErrorResponse",
@@ -383,9 +309,19 @@ async function handleThermostatControl(request, context) {
 
 }
 
-
 function sendResponse(response)
 {
-    log("DEBUG: ", "Lambda response:\n", response);
+    log("Lambda response:\n", response);
     return response
+}
+
+function sendErrorResponse(type, message) {
+    let alexaErrorResponse = new AlexaResponse({
+        "name": "ErrorResponse",
+        "payload": {
+            "type": type,
+            "message": message
+        }
+    });
+    return sendResponse(alexaErrorResponse.get());
 }
