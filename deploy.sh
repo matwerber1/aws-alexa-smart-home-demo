a24126f16467cfb08b74cf008048e8e7d0065e07
Ram Rohit Gannavarapu <ganna.ramu@gmail.com>
2020-08-06 19:47:01 -0600

    updated skill ID

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 BUCKET=<your_bucket_name>
 STACK_NAME=alexa-smart-home-demo
 
-ALEXA_SKILL_ID=<your_alexa_skill_id>
+ALEXA_SKILL_ID=amzn1.ask.skill.427f703f-4494-4c5a-90b3-7095119accc8
 ALEXA_VENDOR_ID=M1HA2HEQHZ2O67
 
 # Install Lambda function dependencies
